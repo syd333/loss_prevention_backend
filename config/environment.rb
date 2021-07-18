3481565ddbdd62c14e374ef9a42aa55bacb4537d
syd333 <sydmiranda@icloud.com>
2021-07-18 19:49:27 -0400

    more environment + gems install

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.active_storage.service = :google
+# config.active_storage.service = :google
