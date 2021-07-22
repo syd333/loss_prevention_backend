5150aa1a804ef47c2e1a6ed4b28b36ad834d3e06
syd333 <sydmiranda@icloud.com>
2021-07-22 13:24:03 -0400

    models set up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_22_171333) do
+ActiveRecord::Schema.define(version: 2021_07_22_171938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2021_07_22_171333) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "post_id", null: false
+    t.string "commentable_type"
+    t.integer "commentable_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
