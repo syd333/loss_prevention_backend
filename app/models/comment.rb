1ed7545e95b9c030d31123e696a5825e07d82477
syd333 <sydmiranda@icloud.com>
2021-07-28 13:58:26 -0400

    application controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  # belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true
   has_many :poly_comments, as: :commentable, dependent: :destroy
 
-  # accepts_nested_attributes_for :commentable
+  accepts_nested_attributes_for :commentable
 end
