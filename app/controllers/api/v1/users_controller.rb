e13911a36eb784fef119f5632e4bf9d106f8184d
syd333 <sydmiranda@icloud.com>
2021-07-28 15:15:45 -0400

    serializers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,33 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create, :index, :show]
+    # user update? 
+
+    def index 
+        users = User.all
+        render json: users 
+      end
+    
+    # def getuser
+    #     render json: {user: UserSerializer.new(current_user)}, status: :accepted
+    # end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def create 
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = issue_token({user_id: @user.id})
+            render json: {user: UserSerializer.new(@user), jwt: @token}, status: :created
+        else 
+            render json: {error: 'failed to create user'}, status: :not_acceptable
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:id, :username, :email, :bio, :password)
+    end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :username, :email, :bio, :password_digest, :liked_posts, :friendships, :inverse_friendships
+
+  has_many :posts
+
 end
