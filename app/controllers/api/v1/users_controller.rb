462e7ef454b3f53d232379c9aba1a192743282db
syd333 <sydmiranda@icloud.com>
2021-08-10 13:29:55 -0400

    test

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
     # skip_before_action :authorized, only: [:create, :index, :show]
     # user update? 
 
